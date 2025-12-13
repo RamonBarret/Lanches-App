9d1794e474ac8ee1b62e7bb6b7346615e7bd7596
Ramon Barreto <ramon_barreto_medrado@hotmail.com>
2025-12-13 17:41:30 -0300

    add class Lanche and Categoria

---
+++ Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿namespace LanchesApp.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        //ForeignKey - relacionamento entre as Classes 'Lanche' e 'Categoria' (um Lanche pode ter muitas Categorias)
+        public List<Lanche> Lanches { get; set; }
+    }
+}
---
+++ Models/Lanche.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesApp.Models
+{
+    public class Lanches
+    {
+        public int LanchesId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImageUrl { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        //ForeignKey - relacionamento entre as Classes 'Lanches' e 'Categoria' (Lanches podem ter muitas categorias)
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}

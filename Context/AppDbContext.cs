ae61ffd874574577b6176e02d544dacb487ec079
Ramon Barreto <ramon_barreto_medrado@hotmail.com>
2025-12-13 17:39:51 -0300

    add class DbContext and add the ConnectionString

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using LanchesApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesApp.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanches> Lanches { get; set; }
+
+
+    }
+}

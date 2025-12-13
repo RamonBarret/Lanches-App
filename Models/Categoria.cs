526d23e6191cb621a007856e6c4898ff7d3c5128
Ramon Barreto <ramon_barreto_medrado@hotmail.com>
2025-12-13 19:35:09 -0300

    create Data Annotations

--- Models/Categoria.cs
+++ Models/Categoria.cs
@@ -1,9 +1,22 @@
-﻿namespace LanchesApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesApp.Models
 {
+    [Table("Categorias")]
     public class Categoria
     {
+        [Key]
         public int CategoriaId { get; set; }
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo é 100 caracteres")]
+        [Required(ErrorMessage = "Informe o nome da categoria")]
+        [Display(Name = "Nome")]
         public string CategoriaNome { get; set; }
+
+        [StringLength(200, ErrorMessage = "O tamanho máximo é 200 caracteres")]
+        [Required(ErrorMessage = "Informe a descrição da categoria")]
+        [Display(Name = "Nome")]
         public string Descricao { get; set; }
 
         //ForeignKey - relacionamento entre as Classes 'Lanche' e 'Categoria' (um Lanche pode ter muitas Categorias)
